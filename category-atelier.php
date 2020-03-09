15f479542a5a568ca1f31bee4a7b9e3982d32a5e
Gabriel Levesque-Lemoyne <gabriel.levesque.lemoyne@hotmail.com>
2020-03-09 11:57:31 -0400

    question4

--- category-atelier.php
+++ category-atelier.php
@@ -35,17 +35,33 @@ get_header();
             $query1 = new WP_Query( $args );
             while ( $query1->have_posts() ) :
                 $query1->the_post();
-                echo '<h3>' . $i++ . '.' . get_the_title() . '___' . '<span class="ateliers_num">' . get_post_field('post_name') . '</span>' . '___' . '<span class="ateliers_auteur">' . get_the_author_meta('display_name', $post->post_author) . '</span>' . '</h3>';
+				echo '<h3>' . $i++ . '.' . get_the_title() . '___' . '<span class="ateliers_num">' . get_post_field('post_name') . '</span>' . '___' . '<span class="ateliers_auteur">' 
+				. get_the_author_meta('display_name', $post->post_author) . '</span>' . '</h3>';
 				/*
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
 				//get_template_part( 'template-parts/content', get_post_type() );
-
+			endwhile;
+			echo '<h1>' . "Bienvenue au tableau des Ateliers" . '</h1>';
+			echo '<h2>' . category_description( get_category_by_slug( 'Atelier' )). '</h2>';
+				$args2 = array(
+					"category_name" => "Atelier",
+					"posts_per_page" => 16, 
+					"orderby" => "post_name",
+					"order" => "ASC"
+				);
+			   
+				$query2 = new WP_Query( $args2 );
+				while ( $query2->have_posts() ) :
+					$query2->the_post();
+					echo '<div class="ateliers_tab">' . '<h3>' . '<div class="ateliers_titre">' . get_the_title() . '</div>' . '<div class="ateliers_noms">' . get_post_field('post_name') . '</div>' . '<div class="ateliers_prof">' 
+				. get_the_author_meta('display_name', $post->post_author) . '</div>' . '</h3>';
+				echo '</div">';
 			endwhile;
 		else :
-
+			
 			get_template_part( 'template-parts/content', 'none' );
 
 		endif;
